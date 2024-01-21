cd5cce0e076180b8c7838d2b518613d57eb69131
Jake Goldman <jgoldman@caltech.edu>
2024-01-20 19:10:16 -0800

    Add analysis for GR QC data

--- HW2/analyze_gr_qc.py
+++ HW2/analyze_gr_qc.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 
 
-# Load CSV into a Pandas DataFrame
-df = pd.read_csv("HW2/network.csv")
+file_path = "HW2/gr_qc_coauthorships.txt"
+columns = ['source', 'target']
+
+df = pd.read_csv(file_path, sep=' ', header=None, names=columns)
 
 G: nx.Graph = nx.from_pandas_edgelist(df, 'source', 'target', create_using=nx.DiGraph)
 
@@ -16,24 +18,24 @@ plt.figure(figsize=(12, 6))
 
 # Outdegree histogram
 plt.subplot(1, 2, 1)
-plt.hist(outdegrees.values(), bins=range(0, 250, 10), color='blue', edgecolor='black')
+plt.hist(outdegrees.values(), bins=range(0, 40, 1), color='blue', edgecolor='black')
 plt.title('Outdegree')
 plt.xlabel('Outdegree')
 plt.ylabel('Frequency')
-plt.xlim(0, 250)
-plt.ylim(0, 1000)
+plt.xlim(0, 40)
+plt.ylim(0, 2000)
 
 # Indegree histogram
 plt.subplot(1, 2, 2)
-plt.hist(indegrees.values(), bins=range(0, 250, 10), color='green', edgecolor='black')
+plt.hist(indegrees.values(), bins=range(0, 40, 1), color='green', edgecolor='black')
 plt.title('Indegree')
 plt.xlabel('Indegree')
 plt.ylabel('Frequency')
-plt.xlim(0, 250)
-plt.ylim(0, 1000)
+plt.xlim(0, 40)
+plt.ylim(0, 2000)
 
 plt.tight_layout()
-plt.savefig("HW2/imgs/histogram.png")
+plt.savefig("HW2/imgs/gr_qc_histogram.png")
 
 G_u: nx.Graph = G.to_undirected()
 
@@ -84,9 +86,9 @@ plt.loglog(np.arange(1, len(in_degree_ccdf) + 1), in_degree_ccdf, label='In-degr
 plt.xlabel('Degree')
 plt.ylabel('Complementary Cumulative Distribution Function (CCDF)')
 plt.legend()
-plt.savefig("HW2/imgs/ccdf.png")
+plt.savefig("HW2/imgs/gr_qc_ccdf.png")
 
-fp = open("HW2/output_data.txt", "w")
+fp = open("HW2/gr_qc_output_data.txt", "w")
 fp.write(f"Max diameter: {max_diameter}\n")
 fp.write(f"Avg diameter: {avg_diameter}\n")
 fp.write(f"Clustering Coefficient: {clustering}\n")
